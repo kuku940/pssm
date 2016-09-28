e22059dfeb503b13e0efc0fab5dcc13a368ed81a
kuku940 <roingeek@qq.com>
2016-09-29 00:55:43 +0800

    mybatis测试类

--- src/test/java/cn/xiaoyu/ssm/spring/TestMybatisConn.java
+++ src/test/java/cn/xiaoyu/ssm/spring/TestMybatisConn.java
@@ -1,8 +1,11 @@
 package cn.xiaoyu.ssm.spring;
 
 import cn.xiaoyu.ssm.dao.UserDao;
+import cn.xiaoyu.ssm.domain.User;
+import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,8 +13,13 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import java.io.IOException;
+import java.io.InputStream;
+
 /**
  * Created by Administrator on 2016/9/28.
+ * 两个核心的类：SqlSessionFactoryBean和MapperFactoryBean
+ *
  */
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = {"classpath*:beans*.xml"})
@@ -26,5 +34,19 @@ public class TestMybatisConn {
 
         UserDao userDao = session.getMapper(UserDao.class);
         System.out.println(userDao.getUserById(1));
+    }
+
+    @Test
+    public void getUserDaoWithoutSpring() throws IOException {
+        String resource = "mapper/UserMapper.xml";
+        InputStream inputStream = Resources.getResourceAsStream("mapper/UserMapper.xml");
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+        SqlSession session = sqlSessionFactory.openSession();
+        try {
+            User user = session.selectOne("cn.xiaoyu.ssm.dao.UserDao.getUserById",1);
+            System.out.println(user);
+        }finally {
+            session.close();
+        }
     }
 }
